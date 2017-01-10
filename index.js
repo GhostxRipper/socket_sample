0d2c303b1cd6f2a67f6df8be8df7aebb4fae7ae8
Yann Amsellem <yann_ams@icloud.com>
2017-01-10 18:31:50 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const { Server } = require('ws')
+const http = require('http')
+const fs = require('fs')
+
+const server = http.createServer((request, response) => {
+  const index = fs.readFileSync('public/index.html')
+  response.writeHead(200, {'Content-Type': 'text/html'})
+  response.end(index)
+})
+
+const webSocket = new Server({ port: 3000 })
+
+webSocket.broadcast = (data) => webSocket.clients.forEach( client => client.send(data) )
+
+webSocket.on('connection', (socket) => {
+  socket.on('message', (data) => {
+    socket.send(JSON.stringify({ event: 'message', data:data }))
+  })
+
+  const id = setInterval(() => {
+    socket.send(JSON.stringify({ event: 'recuriveMessage', data: 'yolo' }))
+  }, 2500) // 2,5 second
+
+  socket.on('close', () => clearInterval(id))
+})
+
+server.listen(6001)
+console.log('WebSocket Server run on 127.0.0.1:3000')
+console.log('HTTP Server run on 127.0.0.1:6001')
